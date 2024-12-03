8aa09cf65f565030d9403826a57392039458923c
Harrison R <harrison.ratcliffe@me.com>
2024-12-03 23:07:13 +0000

    Update join-swarm-cluster.sh

--- spin/join-swarm-cluster.sh
+++ spin/join-swarm-cluster.sh
@@ -2,10 +2,10 @@
 
 # Prompt for necessary variables
 read -p "Enter the Manager IP Address: " MANAGER_IP
-read -p "Should this node be a master (y/n)? " IS_MASTER
+read -p "Should this node be a manager (y/n)? " IS_MANAGER
 
 # Ask for the Swarm cluster token based on the node type
-if [[ "$IS_MASTER" == "y" || "$IS_MASTER" == "Y" ]]; then
+if [[ "$IS_MANAGER" == "y" || "$IS_MANAGER" == "Y" ]]; then
     read -p "Enter the Swarm cluster token (you can get the token from the manager with 'docker swarm join-token manager'): " SWARM_TOKEN
 else
     read -p "Enter the Swarm cluster token (you can get the token from the manager with 'docker swarm join-token worker'): " SWARM_TOKEN
@@ -15,8 +15,8 @@ echo "Starting the process to add this node to the Docker Swarm cluster..."
 echo ""
 
 # Add the node to the Docker Swarm
-if [[ "$IS_MASTER" == "y" || "$IS_MASTER" == "Y" ]]; then
-    echo "This node will be added as a master..."
+if [[ "$IS_MANAGER" == "y" || "$IS_MANAGER" == "Y" ]]; then
+    echo "This node will be added as a manager..."
     sudo docker swarm join --advertise-addr $MANAGER_IP:2377 --token $SWARM_TOKEN
 else
     echo "This node will be added as a worker..."
