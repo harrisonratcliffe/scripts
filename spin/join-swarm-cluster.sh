6353bbca2d65241f95f9edcf618852b44f47d498
Harrison R <harrison.ratcliffe@me.com>
2024-12-17 21:48:00 +0000

    Update join-swarm-cluster.sh

--- spin/join-swarm-cluster.sh
+++ spin/join-swarm-cluster.sh
@@ -5,10 +5,10 @@ read -p "Enter the Manager IP Address: " MANAGER_IP
 read -p "Should this node be a manager (y/n)? " IS_MANAGER
 
 # Ask for the Swarm cluster token based on the node type
-if [[ "$IS_MANAGER" == "y" || "$IS_MANAGER" == "Y" ]]; then
-    read -p "Enter the Swarm cluster token (you can get the token from the manager with 'docker swarm join-token manager'): " SWARM_TOKEN
+if [[ "$IS_MANAGER" == "y" || "$IS_MANAGER" == "Y" || "$IS_MANAGER" == "yes" || "$IS_MANAGER" == "YES" ]]; then
+    read -p "Enter the Swarm cluster token (you can get the token from the manager with \"docker swarm join-token manager | grep 'docker swarm join --token' | awk '{print $5}'\"): " SWARM_TOKEN
 else
-    read -p "Enter the Swarm cluster token (you can get the token from the manager with 'docker swarm join-token worker'): " SWARM_TOKEN
+    read -p "Enter the Swarm cluster token (you can get the token from the manager with \"docker swarm join-token worker | grep 'docker swarm join --token' | awk '{print $5}'\"): " SWARM_TOKEN
 fi
 
 echo "Starting the process to add this node to the Docker Swarm cluster..."
